d603d2f79cb83023e092d57083d9c39177097e8f
arades79 <arades79@gmail.com>
2018-11-10 22:21:46 -0500

    rename variable

--- include/buttons.h
+++ include/buttons.h
@@ -6,7 +6,7 @@
 
 #include <switch.h>
 
-// functions to handle button presses 
+// functions to handle kDown presses 
 
 
 s8 buttonHandler(void)
@@ -16,44 +16,44 @@ s8 buttonHandler(void)
 
     // hidKeysDown returns information about which buttons have been
     // just pressed in this frame compared to the previous one
-    u64 button = hidKeysDown(CONTROLLER_P1_AUTO);
+    u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
 
-    if (button & KEY_PLUS)
+    if (kDown & KEY_PLUS)
         return -1;
-    else if (button & KEY_DUP)
+    else if (kDown & KEY_DUP)
         printf("d-up ");
 
-    else if (button & KEY_DOWN)
+    else if (kDown & KEY_DOWN)
         printf("d-down ");
 
-    else if (button & KEY_LEFT)
+    else if (kDown & KEY_LEFT)
         printf("d-left ");
 
-    else if (button & KEY_RIGHT)
+    else if (kDown & KEY_RIGHT)
         printf("d-right ");
 
-    else if (button & KEY_A)
+    else if (kDown & KEY_A)
         printf("A ");
 
-    else if (button & KEY_B)
+    else if (kDown & KEY_B)
         printf("B ");
 
-    else if (button & KEY_Y)
+    else if (kDown & KEY_Y)
         printf("Y ");
 
-    else if (button & KEY_X)
+    else if (kDown & KEY_X)
         printf("X ");
 
-    else if (button & KEY_R)
+    else if (kDown & KEY_R)
         printf("R ");
 
-    else if (button & KEY_L)
+    else if (kDown & KEY_L)
         printf("L ");
 
-    else if (button & KEY_ZR)
+    else if (kDown & KEY_ZR)
         printf("ZR ");
 
-    else if (button & KEY_ZL)
+    else if (kDown & KEY_ZL)
         printf("ZL ");
     else 
         return 0;
